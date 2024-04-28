6f1e1080ca34e91e155c8c539766da23c930fe97
Luka <odak.skywalker@gmail.com>
2024-04-28 18:42:50 +0200

    described endpoints

--- src/main/java/com/expresion/evaluator/controller/ExpressionController.java
+++ src/main/java/com/expresion/evaluator/controller/ExpressionController.java
@@ -4,6 +4,11 @@ import com.expresion.evaluator.exception.InvalidExpressionException;
 import com.expresion.evaluator.model.Expression;
 import com.expresion.evaluator.service.ExpressionService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +30,23 @@ public class ExpressionController {
     }
 
     @PostMapping("/create")
-    @Operation(summary = "Create a new expression", description = "Provide a name and value for the expression")
+    @Operation(summary = "Create a new expression", description = "Provide a name and value for the expression",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Name and value of the expression to create",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(
+                                    name = "CreateExpressionExample",
+                                    value = "(customer.firstName == \"JOHN\" && customer.salary < 100) OR (customer.address != null && customer.address.city == \"Washington\")"
+                            ),
+                            schema = @Schema(implementation = Expression.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful creation of expression"),
+                    @ApiResponse(responseCode = "400", description = "Invalid expression data provided")
+            }
+    )
     public ResponseEntity<?> createExpression(@RequestParam String name, @RequestBody String value) {
         log.debug("Received expression with name: {} and value: {}", name, value);
         try {
@@ -37,7 +58,45 @@ public class ExpressionController {
     }
 
     @PostMapping("/evaluate/{id}")
-    @Operation(summary = "Evaluate an expression", description = "Provide expression ID and JSON object to evaluate against")
+    @Operation(summary = "Evaluate an expression", description = "Provide expression ID and JSON object to evaluate against",
+            parameters = {
+                    @Parameter(name = "id", description = "The ID of the expression to evaluate", required = true,
+                            example = "123")
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "JSON object used to provide data for evaluating the expression",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "EvaluateExpressionExample",
+                                    value = "{\"customer\": {\"firstName\": \"JOHN\", \"lastName\": \"DOE\", \"address\": {\"city\": \"Chicago\", \"zipCode\": 1234, \"street\": \"56th\", \"houseNumber\": 2345}, \"salary\": 99, \"type\": \"BUSINESS\"}}"
+                            ),
+                            schema = @Schema(implementation = Map.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Expression evaluated successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = @ExampleObject(
+                                            name = "EvaluationSuccessExample",
+                                            value = "{\"result\": true}"
+                                    )
+                            )),
+                    @ApiResponse(responseCode = "400", description = "Invalid input or expression not found",
+                            content = @Content(mediaType = "application/json",
+                                    examples = @ExampleObject(
+                                            name = "EvaluationErrorExample",
+                                            value = "{\"error\": \"Expression ID not found or invalid JSON data\"}"
+                                    ))),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                            content = @Content(mediaType = "application/json",
+                                    examples = @ExampleObject(
+                                            name = "InternalErrorExample",
+                                            value = "{\"error\": \"Unexpected error occurred\"}"
+                                    )))
+            }
+    )
     public ResponseEntity<?> evaluateExpression(@PathVariable("id") Long id, @RequestBody Map<String, Object> jsonData) {
         log.debug("Received expression to evaluate: {}", jsonData);
         try {
